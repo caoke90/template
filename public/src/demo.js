2fdcf8d2c08b00c9289150c7136d68bb5b826c18
caoke90 <914890674@qq.com>
2015-08-10 23:27:31 +0800

    d
    d

--- public/src/demo.js
+++ public/src/demo.js
@@ -1,22 +1,40 @@
 define(function(require, exports, module) {
     require("../css/demo.css")
+    require("../css/bootstrap.min.css")
+    require("dragula.css")
+    require("dragula")
     var EventProxy=require("eventproxy")
     var ep = new EventProxy();
     var $ = require('jquery');
 
-    var tpl=$("template").html()
+    var tpl=$(".template").text()
 
     var data={
         name:21
     }
-    ep.all("tpl","data",function(tpl,data){
-
+    ep.tail("tpl","data",function(tpl,data){
         var str=tpl.replace(/<%=(\w+)%>/g,function(m,p1){
-            cc.log(m)
+
             return data[p1]
         })
-        cc.log(str)
         $("body").append(str)
+
+        dragula([$('#left1')[0], $('#right1')[0]],{copy:true})
+            .on("cloned",function(copy,ele){
+                cc.log("cloned")
+                $(copy).html("cloned")
+                cc.log(copy)
+                cc.log(ele)
+            })
+            .on("cancel",function(el,container,handle){
+                //cc.log(el)
+                //cc.log(container)
+                //cc.log(handle)
+            })
+            .on("drop",function(el,container,handle){
+                //cc.log(el)
+                //cc.log(handle)
+            })
     })
 
     ep.emit("tpl",tpl)
