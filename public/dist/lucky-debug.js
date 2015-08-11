6b5b9883eed97685eb8ec0cfa3c13cc596a29332
caoke90 <914890674@qq.com>
2015-08-11 18:38:12 +0800

    d

--- public/dist/lucky-debug.js
+++
@@ -1,4 +0,0 @@
-define("donut/rename/1.0.0/lucky-debug", [ "jquery-debug", "jquery/jquery.min-debug" ], function(require, exports, module) {
-    var $ = require("jquery-debug");
-    cc.log(21);
-});
--- public/dist/lucky.js
+++
@@ -1,1 +0,0 @@
-define("donut/rename/1.0.0/lucky",["jquery","jquery/jquery.min"],function(a){a("jquery"),cc.log(21)});
